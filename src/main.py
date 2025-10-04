ed77e851cbdf4bd7054dc949cc7f1ceba65e1e5e
Joaquin144 <vvpp898.comco@gmail.com>
2025-10-05 00:29:29 +0530

    install dotenv for secrets

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
+import os
 import time
 import webbrowser
+
+from dotenv import load_dotenv
 
 from src.logger.config import setup_logger
 
 log = setup_logger(__name__)
+load_dotenv()
 
 log.info("Application started")
+
+username = os.getenv("LINKEDIN_EMAIL")
+log.info(f"Application running with credentials for : {username}")
 
 webbrowser.open("https://www.google.com")
 time.sleep(5)
