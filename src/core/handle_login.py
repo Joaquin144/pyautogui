1bbc81beeaa2ef36629fd3c8f5ccebc3c2635c83
Joaquin144 <vvpp898.comco@gmail.com>
2025-10-06 02:22:11 +0530

    implement login by coordinates

--- src/core/handle_login.py
+++ src/core/handle_login.py
@@ -8,7 +8,7 @@ from src.utils.locate_element_with_wait import locate_element_with_wait
 log = setup_logger("handle_login")
 
 
-def handle_login():
+def handle_login_with_image():
     time.sleep(3)
     pyautogui.moveTo(600, 600)
     sso_point = locate_element_with_wait("./resources/login_sso.png")
@@ -23,3 +23,13 @@ def handle_login():
             log.error("unexpected state occurred")
     else:  # could be more better check
         log.info("No login sso found. Skipping login step")
+
+
+def handle_login_with_coordinates():
+    time.sleep(3)
+    pyautogui.moveTo(600, 600)
+    pyautogui.click(349, 412)
+    time.sleep(2)
+    pyautogui.moveTo(600, 600)
+    pyautogui.click(674, 474)
+    time.sleep(4)
