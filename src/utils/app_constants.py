ec404cbc9be256753793b435510eb7976f4fe84f
Joaquin144 <vvpp898.comco@gmail.com>
2025-10-06 02:21:58 +0530

    fix base url

--- src/utils/app_constants.py
+++ src/utils/app_constants.py
@@ -2,5 +2,5 @@
 # base_url_search_prefix = 'https://www.youtube.com/@'
 
 
-website_url = 'https://www.youtube.com/'
+website_url = 'https://www.linkedin.com'
 base_url_search_prefix = 'https://www.linkedin.com/in/'
