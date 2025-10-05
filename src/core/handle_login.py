36d8186f48530511fe35b93373d495450b2bc596
Joaquin144 <vvpp898.comco@gmail.com>
2025-10-06 02:08:41 +0530

    implement login functionality

--- src/core/handle_login.py
+++ src/core/handle_login.py
@@ -1,2 +1,25 @@
+import time
+
+import pyautogui
+
+from src.logger.config import setup_logger
+from src.utils.locate_element_with_wait import locate_element_with_wait
+
+log = setup_logger("handle_login")
+
+
 def handle_login():
-    pass
+    time.sleep(3)
+    pyautogui.moveTo(600, 600)
+    sso_point = locate_element_with_wait("./resources/login_sso.png")
+    if sso_point is not None:
+        log.info(f"Found login sso at {sso_point}")
+        pyautogui.click(sso_point)
+        email_sso_point = locate_element_with_wait("./resources/login_sso_email.png")
+        if email_sso_point is not None:
+            log.info(f"Found login sso email at {email_sso_point}")
+            pyautogui.click(email_sso_point)
+        else:
+            log.error("unexpected state occurred")
+    else:  # could be more better check
+        log.info("No login sso found. Skipping login step")
