5f87825343143346f75959e68cf7d10a96e7a2c7
Joaquin144 <vvpp898.comco@gmail.com>
2025-10-06 00:26:09 +0530

    linkedin poc script

---
+++ src/linkedin_test.py
@@ -0,0 +1,30 @@
+import pyautogui
+from dotenv import load_dotenv
+
+from src.core.extract_profile_info import extract_profile_info
+from src.core.handle_login import handle_login
+from src.core.start_linkedin_op import generate_search_url_for_username, open_linkedin
+from src.logger.config import setup_logger
+from src.utils.key_presses import focus_address_bar
+
+log = setup_logger("linkedin_test")
+load_dotenv()
+
+log.info("POC started")
+
+# open LinkedIn
+open_linkedin()
+
+# username = "aniketkagarwal"
+# username = "someetsingh"
+username = "vibhu"
+
+search_url = generate_search_url_for_username(username)
+focus_address_bar()
+log.debug(f'searching : {search_url}')
+pyautogui.write(search_url)
+pyautogui.press("enter")
+
+log.info(extract_profile_info(username, "info.png"))
+
+log.error("POC done")
