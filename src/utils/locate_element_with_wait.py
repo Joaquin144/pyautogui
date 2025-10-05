43797287552675a0f7ffb9753ec15f4fc36eb03d
Joaquin144 <vvpp898.comco@gmail.com>
2025-10-06 02:12:15 +0530

    handle error in locate_element_with_wait

--- src/utils/locate_element_with_wait.py
+++ src/utils/locate_element_with_wait.py
@@ -4,16 +4,21 @@ import pyautogui
 
 from src.logger.config import setup_logger
 
-log = setup_logger(__name__)
+log = setup_logger("locate_element_with_wait")
 
 
 def locate_element_with_wait(image_path: str, confidence=0.8, timeout_seconds=10, wait_time=0.5):
     start_time = time.time()
     while time.time() - start_time < timeout_seconds:
-        element_location = pyautogui.locateCenterOnScreen(image_path, confidence=confidence)
-        if element_location:
-            end_time = time.time()
-            log.debug(f"Found element in {end_time - start_time} seconds for {image_path}")
-            return element_location
-        time.sleep(wait_time)
+        try:
+            element_location = pyautogui.locateCenterOnScreen(image_path, confidence=confidence)
+            if element_location:
+                end_time = time.time()
+                log.debug(f"Found element in {end_time - start_time} seconds for {image_path}")
+                return element_location
+            time.sleep(wait_time)
+        except pyautogui.ImageNotFoundException as e:
+            log.error(f"Image not found : {e}")
+            pyautogui.scroll(-10)
+            continue
     return None
